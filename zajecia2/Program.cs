b0cd31a384dc8c9dd2e1b95c6490fb5c6e48d379
angelm1974 <mariusz980@gmail.com>
2023-03-07 13:20:40 +0100

    zmiana w funkcji

--- zajecia2/Program.cs
+++ zajecia2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,38 @@ namespace zajecia2
     {
         static void Main(string[] args)
         {
-            /* Mój pierwszy program napisany w języku C# */
-            Console.WriteLine("Witaj Świecie");
-            Console.WriteLine("12abc");
-            Console.ReadKey();
+            Rectangle r1 = new Rectangle();
+            r1.WyswietlPodsumowanie();
+          
+            
+            Console.ReadLine();
+        }
+    }
+
+    class Rectangle
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+
+        public Rectangle()
+        {
+            Console.WriteLine("Podaj pierwszy bok: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugi bok: ");
+            y = double.Parse(Console.ReadLine());
+        }
+
+        double CalculateRectangleField()
+        {
+            return x * y;
+        }
+
+        public void WyswietlPodsumowanie()
+        {
+            Console.WriteLine("Długość: {0}", x);
+            Console.WriteLine("Szerokość: {1}", y);
+            Console.WriteLine("Rectangle field : " + CalculateRectangleField());
         }
     }
 }
